70bcb7fd9a5a54edc19e7094d88df69951131549
andresdev <andresaltamiranda95@gmail.com>
2024-08-30 16:32:26 -0500

    agregamos menuitems y lista

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+// Forma de exponer solo IconData del package de material
+//import 'package:flutter/material.dart' show IconData;
+
+
+
+class MenuItem {
+
+  final String title;
+  final String subtitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({
+    required this.title, 
+    required this.subtitle, 
+    required this.link, 
+    required this.icon
+  });  
+}
+
+
+const appMenuItems = <MenuItem>[
+
+  MenuItem(
+    title: 'Botones', 
+    subtitle: 'Varios botones en Flutter', 
+    link: '/buttons', 
+    icon: Icons.smart_button_outlined
+  ),
+
+  MenuItem(
+    title: 'Tarjetas', 
+    subtitle: 'Contenedor estilizado', 
+    link: '/card', 
+    icon: Icons.credit_card
+  ),
+
+];
+
+
+
+
